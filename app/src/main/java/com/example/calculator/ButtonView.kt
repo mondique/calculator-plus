7d08d87f8fceb0a3152b4e0bd892154db9c7985d
mondique <koganeser@gmail.com>
2020-04-03 10:45:16 +0300

    fix input buttons eating input

--- app/src/main/java/com/example/calculator/ButtonView.kt
+++ app/src/main/java/com/example/calculator/ButtonView.kt
@@ -10,7 +10,7 @@ import android.widget.*
 
 class ButtonView(context: Context, attrs: AttributeSet) :
     RelativeLayout(context, attrs) {
-    
+
     fun getTextView(): TextView =
         getChildAt(0) as TextView
 
@@ -18,10 +18,6 @@ class ButtonView(context: Context, attrs: AttributeSet) :
         override fun onDown(e: MotionEvent): Boolean {
             val v = activity.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
             v.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE))
-            return true
-        }
-
-        override fun onSingleTapUp(e: MotionEvent): Boolean {
             val buttonText = getTextView().text
             val inputView = activity.findViewById<EditText>(R.id.input)
             val inputText = inputView.getText().toString()
